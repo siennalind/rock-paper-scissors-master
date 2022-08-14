8d45bfbbcab804ebc043d44b76b07881d8847c94
siennalind <sienna.lebeaulind@gmail.com>
2022-08-14 11:36:17 +0100

    added score card

--- rock-paper-scissors/src/components/Header.js
+++ rock-paper-scissors/src/components/Header.js
@@ -1,7 +1,10 @@
+import ScoreCard from './ScoreCard';
+
 function Header () {
     return (
         <header>
             <h1>Rock Paper Scissors</h1>
+            <ScoreCard />
         </header>
     )
 }
---
+++ rock-paper-scissors/src/components/ScoreCard.js
@@ -0,0 +1,10 @@
+function ScoreCard () {
+    return (
+        <div className="score-card">
+            <p className="score-title">Score</p>
+            <p className="score-number">0</p>
+        </div>
+    )
+}
+
+export default ScoreCard
