00244d5075342d46dab65fb6422b3105c775d6d2
siennalind <sienna.lebeaulind@gmail.com>
2022-08-15 13:48:07 +0100

    basic game icon style

---
+++ rock-paper-scissors/src/components/Options/GameIcon.js
@@ -0,0 +1,12 @@
+function GameIcon () {
+    return (
+        <div className="game-icon">
+            <div className="icon-border">
+                <div className="icon-background">
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default GameIcon
