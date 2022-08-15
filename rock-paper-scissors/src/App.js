002303206d1ae55e65b436351c53c3488d03cae4
siennalind <sienna.lebeaulind@gmail.com>
2022-08-15 12:04:38 +0100

    moved & styled attribution

--- rock-paper-scissors/src/App.js
+++ rock-paper-scissors/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 
+import Header from './components/Header';
+import Attribution from './components/Attribution';
+
 function App() {
   return (
-    <></>
+    <>
+    <Header />
+    <Attribution />
+    </>
   )}
 
 export default App;
---
+++ rock-paper-scissors/src/components/Attribution.js
@@ -0,0 +1,10 @@
+function Attribution () {
+    return (
+        <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+        Coded by <a href="https://www.github.com/siennalind">Sienna</a>.
+      </div>
+    )
+}
+
+export default Attribution
